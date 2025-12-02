028d8640343b619649ac82f648a4dbbf2c07860d
Marilizzz <143432705+Marilizzz@users.noreply.github.com>
2025-12-02 16:59:08 -0600

    Refactor vite.config.js for cleaner syntax

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,    // ← Esto permite --host y que funcione en cualquier red
-    port: 5173,    // Puerto por defecto (puedes cambiarlo si quieres)
+    host: true,    
+    port: 5173,   
     strictPort: true,
   },
+
 })
